78dc710a3b63567907d71ce5717df11ef6fe502d
Rasoul <rasoul.am1376@gmail.com>
2019-04-29 18:58:24 +0430

    Built Script

---
+++ test/MyTests.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MyTests {
+	
+    @Test
+    public void EZTest() {
+
+        // assert statements
+        assertTrue(0 == 0);
+    }
+}
+
